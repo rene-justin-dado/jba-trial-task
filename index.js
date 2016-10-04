81edc64a845f4e0a16f870c2292a26058b155ef8
Justin <justindado@yahoo.com>
2016-10-04 20:11:52 +1300

    finished first page

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const app = require('express')()
+const express = require('express')
 const path = require('path')
-
 const PORT = process.env.port || 3001
+const app = express()
+
+app.use(express.static('public'))
 
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
 
 app.get('/', (req, res) => res.render('index.html'))
 
