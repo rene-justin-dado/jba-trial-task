634a34513d99cd70809c47d69dbc766ea83520c1
rene-justin-dado <justindado@yahoo.com>
2016-10-05 21:03:58 +1300

    trying more redirection solutions

---
+++ public/index.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('page-nav').onclick = function () {
+    console.log('This is the nav function')
+    location.href = 'http://localhost:3001/measure-effectiveness-b'
+}
