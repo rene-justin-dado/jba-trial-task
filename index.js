85242ffcdcca70511c6b9d137889a674cb5d9052
Justin <justindado@yahoo.com>
2016-10-09 21:41:47 +1300

    skeleton for page8 done

--- index.js
+++ index.js
@@ -16,5 +16,6 @@ app.get('/measure-effectiveness-b', (req, res) => res.render('index3-b.html'))
 app.get('/brands', (req, res) => res.render('index4.html'))
 app.get('/brand-likes', (req, res) => res.render('index5.html'))
 app.get('/section', (req, res) => res.render('index6.html'))
+app.get('/media-channels', (req, res) => res.render('index7.html'))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
